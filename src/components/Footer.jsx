7644e729d078fe8d7797bef3f2b23bf6b454b81e
Your Name <you@example.com>
2024-09-11 17:52:26 +0100

    tweak

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>I am not a designer.</p>
       <p>Copyright © 2024 Jack Berry. All rights reserved.</p>
     </footer>
   );
